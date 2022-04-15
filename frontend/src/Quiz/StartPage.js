7207194a3926964d199d8f4214d8461ea876bb74
shinitaqu <g1attaqueod@gmail.com>
2022-04-16 00:42:46 +0300

    Start page text centered

--- frontend/src/Quiz/StartPage.js
+++ frontend/src/Quiz/StartPage.js
@@ -4,7 +4,7 @@ import StartButton from "./StartButton";
 export default function StartPage({ onClick }) {
   return (
     <div>
-      <h2>Click button to start quiz</h2>
+      <h2 style={{ textAlign: "center" }}>Click button to start quiz</h2>
       <StartButton onClick={onClick}></StartButton>
     </div>
   );
